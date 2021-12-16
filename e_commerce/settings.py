4f5d14dcf027d910f63cc1f5c8e9e61a62b4960a
DanzabeBoris <mbobeboris1@gmail.com>
2021-12-16 10:44:46 +0100

    Mises à jour 1.0.1

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -12,9 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-of70bw4xpzb1-4rzm2@(k(@%el5zgvct19fpz^n80d!ag^h)+l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
-ALLOWED_HOSTS = ['clivershop.com']
+try:
+    ALLOWED_HOSTS = ['127.0.0.1']
+except:
+    ALLOWED_HOSTS = ['clivershop.com']
 
 
 # Application definition
